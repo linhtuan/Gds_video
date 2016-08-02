a7e749019576f2d6efee5cecf30d644cbf3de387
linh <linhnlt@pixelz.com>
2016-08-02 13:08:30 +0700

    update code pass register

--- Gds.VideoFrontend/App_Start/BundleConfig.cs
+++ Gds.VideoFrontend/App_Start/BundleConfig.cs
@@ -8,24 +8,15 @@ namespace Gds.VideoFrontend
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/base-lib-js")
+               .Include("~/Scripts/jquery-2.1.4.min.js")
+               .Include("~/Scripts/plugins/jquery-ui/jquery-ui.min.js")
+               .Include("~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/SiteAll.css",
+                      "~/Content/site.css",
+                      "~/fonts/css/font-awesome.css"));
         }
     }
 }
